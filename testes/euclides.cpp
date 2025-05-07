57634996ff40f6d408967aae02ddcb82e6e6d6ed
ajbc <ajbc@g2c10.cin.ufpe.br>
2025-05-07 14:50:39 -0300

    ajsao

--- testes/euclides.cpp
+++ testes/euclides.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
-#include <cstdlib>
 using namespace std;
 
-int euclides(int a, int b){
+int euclides(int a, int b, int i=0){
     if (b==0){
         return a;
     }
     else{
-        cout << b << '=' << a << '*' << abs(a/b) << '+' << a%b << endl;
-        return euclides(b, a%b);
-    }
+        if (i>0){
+            cout << b << '=' << a << '*' << a/b << '+' << a%b << endl;
+            return euclides(b, a%b);
+    }}
 }
 
 int main(){
     int p;
     int s;
+    string nome = "a";
     cin >> p;
     cin >> s;
-    cout<<euclides(p, s)<< endl;
+    cout << euclides(p, s) << nome << endl;
     return 0;
 }
